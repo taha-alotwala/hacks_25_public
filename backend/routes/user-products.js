2d047dddf72e7d6ecf47c3a0be3152083e1bdba0
Taha <tahaalotwala@gmail.com>
2025-01-30 06:47:21 +0530

    vendor location refactor

--- backend/routes/user-products.js
+++ backend/routes/user-products.js
@@ -2,12 +2,14 @@ const express = require("express");
 const {
   getProductListings,
   getProductsForVendor,
+  getVendorLocations,
 } = require("../controllers/user-products");
 const { authMiddleware } = require("../middleware/authentication");
 
 const router = express.Router();
 
 router.get("/", authMiddleware, getProductListings);
+router.get("/vendors/locations", authMiddleware, getVendorLocations);
 router.get("/:id", authMiddleware, getProductsForVendor);
 
 module.exports = router;
